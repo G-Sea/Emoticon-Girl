437bf51cd59ee3d83ae123c539a2bd3383b3e809
G-Sea <101786678+G-Sea@users.noreply.github.com>
2023-09-03 18:47:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import random
 在收到GPT接口回复时，检测“:xxx:”格式的表情包表达式，将其替换为Image"
 """
 
+
 # 注册插件
 @register(name="Emoticon-Girl", description="表情包表达式转换", version="0.1", author="cillow")
 class HelloPlugin(Plugin):
@@ -15,38 +16,37 @@ class HelloPlugin(Plugin):
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
         pass
-    
+
     # 当收到GPT回复时触发
     @on(NormalMessageResponded)
     def normal_message_responded(self, event: EventContext, **kwargs):
-        response_text:str = kwargs['response_text']
+        response_text: str = kwargs['response_text']
 
         emotions = {
-    "love":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_20220125101547775.png'
-    ],
-    "sad":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504258.jpg'
-    ],
-    "idc":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_2022012510150485.png'
-    ],
-    "die":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504878.png'
-    ],
-    "sorry":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504618.png'
-    ],
-    "ok":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504835.png'
-    ],
-    "this":[
-        'https://img1.ali213.net/glpic/2022/01/25/584_2022012510150417.png'
-    ],
-    "resist":[
-        'pic/resist.jpg']
-}
-
+            "love": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_20220125101547775.png'
+            ],
+            "sad": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504258.jpg'
+            ],
+            "idc": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_2022012510150485.png'
+            ],
+            "die": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504878.png'
+            ],
+            "sorry": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504618.png'
+            ],
+            "ok": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_20220125101504835.png'
+            ],
+            "this": [
+                'https://img1.ali213.net/glpic/2022/01/25/584_2022012510150417.png'
+            ],
+            "resist": [
+                'pic/resist.jpg']
+        }
 
         import re
         # 后续改一下正则
@@ -57,19 +57,17 @@ class HelloPlugin(Plugin):
             logging.info(emotion)
             print(emotion)
 
-            emotion_dict:dict = emotions
+            emotion_dict: dict = emotions
+
             if emotion in emotion_dict.keys():
                 e_list = emotion_dict[emotion]
                 url = e_list
                 logging.debug('choose emotion: {}'.format(url))
-                img = [response_text.replace(ma.group(), '')]
-                if url.startswith('https'):
-                    img = [Image(url=url), response_text.replace(ma.group(), '')]
+
+                if kwargs["launcher_type"] == "group":
+                    host.send_group_message(kwargs["launcher_id"], Image(url=url), response_text.replace(ma.group(), ''))
                 else:
-                    pass
-                    
-                event.add_return("reply", img)
-                event.prevent_default()
+                    host.send_person_message(kwargs["launcher_id"], Image(url=url), response_text.replace(ma.group(), ''))
 
     # 插件卸载时触发
     def __del__(self):
